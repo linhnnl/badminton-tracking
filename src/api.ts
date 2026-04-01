78fed32217e435ac635f2eee24e552cd6c1640b4
linhnnl <linhnnl@awing.vn>
2026-04-01 21:19:04 +0700

    change api url

--- src/api.ts
+++ src/api.ts
@@ -2,8 +2,7 @@
 import axios from 'axios';
 import type { Member, Session, Attendance, Payment } from './types';
 
-const BASE_URL = 'https://script.google.com/macros/s/AKfycbzOJRA_2lkYvIsGe74GzeNKlx14TaoOARxrg-IMVCILoWdRssWNVG2odcHzv4UVlVp3jg/exec';
-                //https://script.google.com/macros/s/AKfycbzOJRA_2lkYvIsGe74GzeNKlx14TaoOARxrg-IMVCILoWdRssWNVG2odcHzv4UVlVp3jg/exec
+const BASE_URL = 'https://script.google.com/macros/s/AKfycbxTnTX3QXNpkBGvdkf-abu2yx_0Yjo1z5jYjcfWDQGbsjHa46iJUsNr_DshbByEei_4dg/exec';
 
 export const fetchMembers = async (): Promise<Member[]> => {
   const { data } = await axios.get(`${BASE_URL}?type=members`);
